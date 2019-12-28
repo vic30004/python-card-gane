6fb5e61200d04e20e81fed8a8d9dacce7ce868e7
vic30004 <victor_993@msn.com>
2019-12-27 19:13:56 -0800

    initial win and lose func setup

--- app.py
+++ app.py
@@ -6,34 +6,51 @@ playing = True
 def take_bet(chips):
     while True:
         try:
-            chips.bet=int(input("How many chips would like to bet? "))
+            chips.bet = int(input("How many chips would like to bet? "))
         except:
             print("Sorry please provide an integer")
-        
+
         else:
-            if chips.bet >chips.total:
-                print("Sorry you don't have enough chips! You have: {}".format(chips.total))
-            
+            if chips.bet > chips.total:
+                print("Sorry you don't have enough chips! You have: {}".format(
+                    chips.total))
+
             else:
                 break
 
-def hit(deck,hand):
+def hit(deck, hand):
     hand.add_card(deck.deal())
     hand.adjust_for_ace()
 
-def hit_or_stand(deck,hand):
-    global playing 
+
+def hit_or_stand(deck, hand):
+    global playing
 
     while True:
-        x=input('Hit of Stand> Enter h or s')
+        x = input('Hit of Stand> Enter h or s')
 
-        if x[0].lower() =='h':
-            hit(deck,hand)
-        
+        if x[0].lower() == 'h':
+            hit(deck, hand)
+
         elif x[0].lower == 's':
             print("Player Stands, Dealer's Turn")
-            playing=False
+            playing = False
         else:
             print("Sorry, I did not understand that. Please enter h or s only!")
             continue
         break
+
+def player_busts():
+    pass
+
+def player_wins():
+    pass
+
+def dealer_busts():
+    pass
+
+def dealer_wins():
+    pass
+
+def push():
+    pass
