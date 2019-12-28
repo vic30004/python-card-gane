ae912ead49f2afe01b19fe153fd0ddd47f51d389
vic30004 <victor_993@msn.com>
2019-12-27 18:59:11 -0800

    created a function to take a bet

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
 import deck
 
 playing = True
+
+
+def take_bet(chips):
+    while True:
+        try:
+            chips.bet=int(input("How many chips would like to bet? "))
+        except:
+            print("Sorry please provide an integer")
+        
+        else:
+            if chips.bet >chips.total:
+                print("Sorry you don't have enough chips! You have: {}".format(chips.total))
+            
+            else:
+                break
